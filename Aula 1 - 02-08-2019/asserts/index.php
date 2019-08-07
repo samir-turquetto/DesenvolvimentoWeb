4b8fbf984ef571fe1656a92e1d67e5c724601c1f
Samir Turquetto <samir.turquetto@hotmail.com>
2019-08-06 22:38:50 -0300

    Aula-06-08-19

--- Aula 1 - 02-08-2019/asserts/index.php
+++ Aula 1 - 02-08-2019/asserts/index.php
@@ -1,49 +1,133 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tio Sam Store</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tio Sam Store</title>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/site.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/site.css">
 
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>  
-    <script src="js/function.js"></script>    
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/function.js"></script>
 </head>
+
 <body>
-<header class="container">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"> 
-    <img class="logo" src="img/logo.png" title="logo da marvel" alt="logo da marvel"/>
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <header class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img class="logo" src="img/logo.png" title="logo da marvel" alt="logo da marvel" />
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item"><a class="nav-link" href="#">Home </a></li>   
-      <li class="nav-item"><a class="nav-link" href="#">Quem Somos </a></li> 
-      <li class="nav-item"><a class="nav-link" href="#">Produtos </a></li> 
-      <li class="nav-item"><a class="nav-link" href="#">Contato </a></li>  
-    </ul>  
-  </div>
-</nav>
-</header>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item"><a class="nav-link" href="#">Home </a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Quem Somos </a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Produtos </a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Contato </a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 
 
 
-<div id="banner" class="container-fluid">
-    <div class="owl-carousel owl-theme">
-        <div class="item"><img src="./uploads/banners/banner.1.jpg" title="banner vingadores" alt="banner vingadores"/></div>
-        <div class="item"><img src="./uploads/banners/banner.2.jpg" title="banner vingadores" alt="banner vingadores"/></div>
-        <div class="item"><img src="./uploads/banners/banner.3.jpg" title="banner vingadores" alt="banner vingadores"/></div>
+  <div id="banner" class="container-fluid">
+    <div class="owl-carousel-banner owl-carousel owl-theme">
+      <div class="item"><img src="./uploads/banners/banner.1.jpg" title="banner vingadores" alt="banner vingadores" /></div>
+      <div class="item"><img src="./uploads/banners/banner.2.jpg" title="banner vingadores" alt="banner vingadores" /></div>
+      <div class="item"><img src="./uploads/banners/banner.3.jpg" title="banner vingadores" alt="banner vingadores" /></div>
     </div>
-</div>
+  </div>
+
+  <section id="cards" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2>Lista de Produtos</h2>
+        <br />
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="./uploads/banners/banner.1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-danger w100">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="./uploads/banners/banner.2.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-danger w100">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="./uploads/banners/banner.3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-danger w100">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="slide" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2>Lançamentos</h2>
+        <br />
+      </div>
+      <div class="owl-carousel-slide owl-carousel owl-theme">
+        <div class="item">
+          <div class="card">
+            <img src="./uploads/banners/banner.1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-danger w100">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="card">
+            <img src="./uploads/banners/banner.2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-danger w100">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="card">
+            <img src="./uploads/banners/banner.3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-danger w100">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <hr />
 </body>
+
 </html>
--- Aula 1 - 02-08-2019/asserts/js/function.js
+++ Aula 1 - 02-08-2019/asserts/js/function.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.owl-carousel-banner').owlCarousel({
         loop:true,
         margin:0,
         nav:true,
         items:1,
+        dots: true
+    })
+
+    $('.owl-carousel-slide').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:3,
         dots: true
     })
 })
